8770b79e03677f35e713f8c238fbeed3373c95c6
Ernest Kirstein <ekirstein@discoveryco.com>
2014-11-11 16:28:02 -0500

    Updated Passcrack Help

--- passcrack/PasswordCracker.java
+++ passcrack/PasswordCracker.java
@@ -1,16 +1,16 @@
 public class PasswordCracker{
-  public static void main(String[] args) throws Throwable{
-    
-    String pass = "password";
-    System.out.println("Trying password '"+pass+"'");
-    
-    //try the password
-    if(CrackerUtils.tryPassword(pass)){
-      System.out.println("That was the right password!");
+
+    public static void main(String[] args) throws Throwable{
+        String pass = "password";
+        System.out.println("Trying password '"+pass+"'");
+
+        //try the password
+        if(CrackerUtils.tryPassword(pass)){
+            System.out.println("That was the right password!");
+        }
+        else{
+            System.out.println("That's not the right password.");
+        }
     }
-    else{
-      System.out.println("That's not the right password.");
-    }
-    
-  }
+
 }
