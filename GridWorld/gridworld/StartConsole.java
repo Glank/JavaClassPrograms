50da7eff277425e8abd65a035922b967251a2e07
Ernest Kirstein <ekirstein@discoveryco.com>
2014-11-20 17:17:36 -0500

    Added locked mode for gridworld

--- GridWorld/gridworld/StartConsole.java
+++ GridWorld/gridworld/StartConsole.java
@@ -3,8 +3,14 @@ import gridworld.gui.GameConsole;
 import javax.swing.JFrame;
 public class StartConsole{
     public static void main(String[] args){
+        boolean locked = args.length>=1 && args[0].equals("locked");
         GameConsole console = new GameConsole();
-        console.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        if(locked){
+            console.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            console.setAlwaysOnTop(true);
+        }
+        else
+            console.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         console.setVisible(true);
     }
 }
